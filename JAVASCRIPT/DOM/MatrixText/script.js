925682c2f6423627937f61f8ae1d3350b9fdcd3c
Kushagra Awasthi <Kushagraawasthi37@gmail.com>
2025-12-09 12:07:28 +0530

    Matrix Text Generator

---
+++ JAVASCRIPT/DOM/MatrixText/script.js
@@ -0,0 +1,24 @@
+let para = document.querySelector("p");
+const characters = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+const text = para.innerText;
+
+let iteration = 0;
+
+function randomText() {
+  const str = text
+    .split("")
+    .map((char, index) => {
+      if (index < iteration) return char;
+
+      return characters.split("")[
+        Math.floor(Math.random() * characters.length)
+      ];
+    })
+    .join("");
+  para.innerText = str;
+  iteration += 0.25;
+}
+
+para.addEventListener("mouseenter", () => {
+  setInterval(randomText, 40);
+});
