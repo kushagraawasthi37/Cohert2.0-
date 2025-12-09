a7c35e52474403e40a24ce10502dbc889977117a
Kushagra Awasthi <Kushagraawasthi37@gmail.com>
2025-12-09 11:00:10 +0530

    Spotlight effect

---
+++ JAVASCRIPT/DOM/SpotLightEffect/script.js
@@ -0,0 +1,8 @@
+let box = document.querySelector(".box");
+
+box.addEventListener("mousemove", (e) => {
+  console.log(e.clientX, e.clientY);
+
+  document.body.style.setProperty("--x", e.clientX + "px");
+  document.body.style.setProperty("--y", e.clientY + "px");
+});
