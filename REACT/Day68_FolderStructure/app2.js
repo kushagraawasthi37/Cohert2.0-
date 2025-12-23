d9237648062b4c4ccb4a40a6a34032c89fae9156
Kushagra Awasthi <Kushagraawasthi37@gmail.com>
2025-12-23 11:35:38 +0530

    React folder Structure

---
+++ REACT/Day68_FolderStructure/app2.js
@@ -0,0 +1,5 @@
+const app = () => {
+  return React.createElement("div", { id: "box1" }, `This is App`);
+};
+
+export default app;
---
+++ REACT/Day68_FolderStructure/script.js
@@ -0,0 +1,5 @@
+import app from "./app2.js";
+
+const container = document.querySelector("#container");
+const root = ReactDOM.createRoot(container);
+root.render(app());
