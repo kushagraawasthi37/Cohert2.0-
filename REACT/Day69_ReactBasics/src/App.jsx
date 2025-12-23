a2b33a8ef7734520b731ed33d75aff96f034b5c1
Kushagra Awasthi <Kushagraawasthi37@gmail.com>
2025-12-23 12:09:23 +0530

    React basics

---
+++ REACT/Day69_ReactBasics/src/App.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import Component from "./components/test";
+const App = () => {
+  return (
+    <div>
+      <h1>App</h1>
+      <Component />
+    </div>
+  );
+};
+
+export default App;
