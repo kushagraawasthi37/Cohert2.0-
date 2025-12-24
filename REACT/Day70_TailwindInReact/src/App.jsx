d0fa1a2073be6c70f496b8282e08d3298bf48c26
Kushagra Awasthi <Kushagraawasthi37@gmail.com>
2025-12-24 11:23:32 +0530

    Tailwind in React

---
+++ REACT/Day70_TailwindInReact/src/App.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+const App = () => {
+  return (
+    <div>
+      <h1 className="p-5 m-5 bg-red-600 text-center font-medium text-6xl ">
+        Kushagra Awasthi is here
+      </h1>
+      <h2 className="bg-emerald-700 text-center text-3xl  font-extralight">
+        Tailwind With React
+      </h2>
+    </div>
+  );
+};
+
+export default App;
